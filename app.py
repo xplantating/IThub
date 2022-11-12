75dc0e038239c5a2983a9559c7cff267d4243683
Emil <113785577+GrustnyyVeselchak@users.noreply.github.com>
2022-11-12 19:05:46 +0400

    Routes 1.1

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from flask import Flask, render_template, url_for
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder='./template')
 
 @app.route("/")
-@app.route("index")
 def index():
     return render_template('index.html')
 
