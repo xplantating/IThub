ecf0eb9c503074ab12d147cda103aa528344abb5
xplantating <113053449+xplantating@users.noreply.github.com>
2022-12-23 20:27:44 +0400

    Add files via upload

---
+++ static/js/course.js
@@ -0,0 +1,9 @@
+
+  function backFunction() {
+    var div = document.getElementById("backDIV");
+    if (div.style.display === "block") {
+      div.style.display = "none";
+    } else {
+      div.style.display = "block";
+    }
+  }
